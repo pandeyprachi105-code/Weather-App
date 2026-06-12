095b3b4da199e55eea1c78175f1dec647ef62c88
Prachi Pandey <pandeyprachi105@gmail.com>
2026-06-12 13:23:10 +0530

    script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ async function getWeather() {
     const city = document.getElementById("city").value;
 
     const url =
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=YOUR_API_KEY&units=metric`;
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=04b71770db6a2be15f8786fd29f983b2&units=metric`;
 
     const response = await fetch(url);
     const data = await response.json();
